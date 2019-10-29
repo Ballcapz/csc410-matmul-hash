9204a9acff5a196e2afac861ce50a44d122556d2
Zach Johnson <zlg23johnson@gmail.com>
2019-10-28 20:14:46 -0500

    Hashing done for 4 lowercase letters, half (or more) parallelized with omp

---
+++ hashs.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+unsigned long hash(char *str);
+
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Please provide an argument ./<exe> <hashValue>\n");
+		return 0;
+	}
+
+	char *ptr;
+	unsigned long hashNumber = strtoul(argv[1], &ptr, 10);
+	
+	char resultString[5];
+	unsigned long testRes;
+
+
+
+	for (char i = 97; i <= 122; i++)
+	{
+		for (char j = 97; j <= 122; j++)
+		{
+			for (char k = 97; k <= 122; k++)
+			{
+				for (char l = 97; l <= 122; l++)
+				{
+					char param[5];
+					param[0] = i;
+					param[1] = j;
+					param[2] = k;
+					param[3] = l;
+					param[4] = '\0';
+					
+					testRes = hash(param);
+
+					if (testRes == hashNumber)
+					{
+						resultString[0] = i;
+						resultString[1] = j;
+						resultString[2] = k;
+						resultString[3] = l;
+						printf("%s  <-- string value\n", resultString);
+						return 0;
+					}
+
+				} // end l
+			} // end k
+		} // end j
+	} // end i
+
+	return 0;
+}
